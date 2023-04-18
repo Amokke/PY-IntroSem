a92ef24df7518ac0803cf5d44868cc1046081fee
Amokke <24.02.1985@mail.ru>
2023-04-18 20:17:10 +0700

    Home work lesson 4

--- 4_Lesson/HW/4_24_task.py
+++ 4_Lesson/HW/4_24_task.py
@@ -1,16 +1,14 @@
 bushes = int(input("Введите колличество кустов: "))
 
-berrybush = {}
+berrybush = list()
 
 for i in range(bushes):
-	berrybush[i+1] = int(input(f"Введите колличество ягод на {i+1} кусте: "))
+	berrybush.append(int(input(f"Введите колличество ягод на {i+1} кусте: ")))
 
-max = 0
-optimal = 0
 
-for bush, berry in berrybush:
-	if max < (berrybush[bush] + berrybush[bush - 1] + berrybush[bush + 1]):
-		max = (berrybush[bush] + berrybush[bush - 1] + berrybush[bush + 1])
-		optimal = bush
+count = list()
 
-print(f"Оптимальные куста для сбора {optimal - 1}{optimal}{optimal + 1}{max}")
+for i in range(bushes -1):
+	count.append((berrybush[i - 1]) + (berrybush[i]) + (berrybush[i + 1]))
+count.append((berrybush[-2]) + (berrybush[-1]) + (berrybush[0]))
+print(max(count))
