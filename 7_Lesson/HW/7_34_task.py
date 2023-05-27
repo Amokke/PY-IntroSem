0e36111ad362bf4cd6c16e9194960c6baf20fc52
Amokke <24.02.1985@mail.ru>
2023-05-27 20:18:06 +0700

    Исправление алгоритма

--- 7_Lesson/HW/7_34_task.py
+++ 7_Lesson/HW/7_34_task.py
@@ -1,17 +1,9 @@
-text = str(input("Введите фразу: "))
+text = input("Введите фразу: ").split()
+dictionary = 'уеыаоэяию'
+result = [sum([True for j in word.lower() if j in dictionary]) for word in text]
 
-def ritm(str):
-    str = str.split()
-    list_w = []
-    for word in str:
-        sum_w = 0
-        for i in word:
-            if i in 'аеёиоуыэюя':
-                sum_w += 1
-        list_w.append(sum_w)
-    return len(list_w) == list_w.count(list_w[0])
-
-if ritm(text):
+print(result)
+if all(result) and len(set(result)) == 1:
     print('Парам пам-пам')
 else:
     print('Пам парам')
